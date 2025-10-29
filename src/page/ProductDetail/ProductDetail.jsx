f3e3282e135448da56c1d1156e8a015e775d9e71
JJH <jeww1234@gmail.com>
2025-10-29 19:01:12 +0900

    sad

--- src/page/ProductDetail/ProductDetail.jsx
+++ src/page/ProductDetail/ProductDetail.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import "./ProductDetail.style.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
