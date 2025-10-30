418550eacea3b11f0332893698153396ac043df1
JJH <jeww1234@gmail.com>
2025-10-31 01:57:24 +0900

    login-style

--- src/component/ProductCard/ProductCard.jsx
+++ src/component/ProductCard/ProductCard.jsx
@@ -3,23 +3,27 @@ import "./ProductCard.style.css";
 import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
-  const navigate = useNavigate()
-  const showDetail =()=>{
-    navigate(`/product/${item.id}`)
-  }
+  const navigate = useNavigate();
+  const showDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
   return (
     <div className="product-card" onClick={showDetail}>
-      <img src={item?.img} alt="" />
-      <h4>{item?.title}</h4>
-      <span className="me-1">{item?.choice === true ? "*Choice" : ""}</span>
-      <span className="me-1">{item?.new === true ? "*신제품" : ""}</span>
-      <select name="" id="" className="my-1">
-        <option value="">사이즈</option>
-        <option value="">S</option>
-        <option value="">M</option>
-        <option value="">L</option>
-      </select>
-      <div>\{item?.price}</div>
+      <div className="product-card-imgBox mb-4">
+        <img src={item?.img} alt="" />
+      </div>
+      <div className="px-4">
+        <h4>{item?.title}</h4>
+        <span className="me-1">{item?.choice === true ? "*Choice" : ""}</span>
+        <span className="me-1">{item?.new === true ? "*신제품" : ""}</span>
+        <select name="" id="" className="my-1">
+          <option value="">사이즈</option>
+          <option value="">S</option>
+          <option value="">M</option>
+          <option value="">L</option>
+        </select>
+        <div>\{item?.price}</div>
+      </div>
     </div>
   );
 };
--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -20,9 +20,6 @@ const Login = ({setAuthenticate }) => {
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
